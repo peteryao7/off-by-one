abb38376ee5eaadc67d2d1c0d5da56c52cab9e4a
peteryao7@gmail.com <peteryao7@gmail.com>
2018-12-12 16:26:49 -0800

    Heroku 5 push attempt

--- off-by-one/frontend/off-by-one.jsx
+++ off-by-one/frontend/off-by-one.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/root';
+import Root from './components/Root';
 import { login, signup, logout } from "./actions/session_actions";
 
 document.addEventListener('DOMContentLoaded', () => {
