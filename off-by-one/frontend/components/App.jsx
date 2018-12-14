d28b6a2fda3e2e4f2d40dc2f2c67e2bb1f24b900
peteryao7@gmail.com <peteryao7@gmail.com>
2018-12-14 10:07:42 -0800

    Revert changes

--- off-by-one/frontend/components/App.jsx
+++ off-by-one/frontend/components/App.jsx
@@ -5,7 +5,7 @@ import GreetingContainer from './Greeting/greeting_container';
 import LoginFormContainer from './SessionForm/login_form_container';
 import SignupFormContainer from './SessionForm/signup_form_container';
 // import QuestionShowContainer from './Question/question_show_container';
-import QuestionFormContainer from './Question/question_form_container';
+// import QuestionFormContainer from './Question/question_form_container';
 import Header from './Header/header';
 import LeftSidebar from './LeftSidebar/left_sidebar'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
